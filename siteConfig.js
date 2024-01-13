450f5a5fd379fd7a8bc9a0dd88e88b99df80eca9
office3rock <105702924+office3rock@users.noreply.github.com>
2024-01-13 08:21:25 +0200

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -18,26 +18,26 @@ module.exports = {
   subscribeWidget: {
     visible: false,
     title: "lata-gouveia",
-    helpText: "הישארו מעודכנים",
-    successMessage: "תודה ההודעה נשלחה בהצלחה",
+    helpText: "Keep following",
+    successMessage: "Thank you The message was sent successfully",
   },
   header: {
     navigation: [
       {
-        label: "דף הבית",
+        label: "Homepage",
         url: "https://lata-gouveia.com/",
       },
       {
-        label: "יצירת קשר",
+        label: "Contact",
         url: "https://lata-gouveia.com/contact",
       },
     ],
   },
   footer: {
-    copyright: "כל הזכויות שמורות ",
+    copyright: "All rights reserved ",
     navigation: [
       {
-        label: "דף הבית",
+        label: "Homepage",
         url: "https://lata-gouveia.com/",
       },
       {
@@ -46,7 +46,7 @@ module.exports = {
       },
       
       {
-        label: "יצירת קשר",
+        label: "Contact",
         url: "https://lata-gouveia.com/contact",
       },
       
